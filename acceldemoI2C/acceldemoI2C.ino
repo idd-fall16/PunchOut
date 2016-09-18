9844b22131c78f2bd8ba4a43444bf3ea85249bc1
Jonya Chen <jc957@cornell.edu>
2016-09-17 23:32:17 -0700

    Final code! (Added libraries too)

--- acceldemoI2C/acceldemoI2C.ino
+++ acceldemoI2C/acceldemoI2C.ino
@@ -17,6 +17,15 @@ SYSTEM_MODE(MANUAL);
 // I2C
 Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 
+//Use flags to keep track of only registering when it's the first press
+int leftAccFlag = 0; //left accelerometer
+int rightAccFlag = 0; //right accelerometer
+int leftFlexFlag = 0; //left flex sensor
+int rightFlexFlag = 0; //right flex sensor
+
+//Sets pins for flex sensors
+int flexSensorLeft = A0;
+int flexSensorRight = A1;
 
 void setup(void) {
   
@@ -33,6 +42,9 @@ void setup(void) {
   
   //Serial.print("Range = "); Serial.print(2 << lis.getRange());  
   //Serial.println("G");
+
+  pinMode(flexSensorLeft, INPUT);    // sets pin as input - left flex sensor
+  pinMode(flexSensorRight, INPUT);   // sets pin as input - right flex sensor
 }
 
 void loop() {
@@ -51,6 +63,102 @@ void loop() {
   Serial.print(" \tY: "); Serial.print(event.acceleration.y); 
   Serial.print(" \tZ: "); Serial.print(event.acceleration.z); 
   Serial.println(" m/s^2 ");
+
+    // Read in values of flex sensors
+  /*
+  int flexPositionLeft;    // Input value from the analog pin for left sensor
+  int flexPositionRight;   // Input value from the analog pin for right sensor
+
+  flexPositionLeft = analogRead(flexSensorLeft);
+  flexPositionRight = analogRead(flexSensorRight);
+  */
+
+  //For debugging
+  //Serial.print("Left flex sensor reading: ");
+  //Serial.println(flexPositionLeft);
+  //Serial.print("Right flex sensor reading: ");
+  //Serial.println(flexPositionRight);
+  
+  //detects which punches have been thrown
+
+  //defend - both go up at same time
+  /*
+  if(event.acceleration.y < -5 && event1.acceleration.y < -5) {
+    if (leftAccFlag == 0 && rightAccFlag == 0) {
+      Serial.println("Defend");
+      leftAccFlag = 1;
+      rightAccFlag = 1;
+    }
+  }
+
+  //duck - both go down at same time
+  else if(event.acceleration.y > 5 && event1.acceleration.y > 5) {
+    if (leftAccFlag == 0 && rightAccFlag == 0) {
+      Serial.println("Duck");
+      leftAccFlag = 1;
+      rightAccFlag = 1;
+    }
+  }
+  */
+
+  //left jab - left goes high
+  if(event.acceleration.y < -5) {
+    if (leftAccFlag == 0) {
+      Serial.println("Left Jab");
+      leftAccFlag = 1;
+    }
+  }
+
+/*
+  //right jab - right goes high
+  else if(event1.acceleration.y < -5) {
+    if (rightAccFlag == 0) {
+      Serial.println("Right Jab");
+      rightAccFlag = 1;
+    }
+  }
+  */
+
+  //left body - left goes low
+  else if(event.acceleration.y > 5) {
+    if (leftAccFlag == 0) {
+      Serial.println("Left Body");
+      leftAccFlag = 1;
+    }
+  }
+
+/*
+  //right body - right goes low
+  else if(event1.acceleration.y > 5) {
+    if (rightAccFlag == 0) {
+      Serial.println("Right Body");
+      rightAccFlag = 1;
+    }
+  }
+  */
+
+/*
+  else if( flexPositionLeft < 3200) {
+    if (leftFlexFlag == 0) {
+      Serial.println("Dodge Left");
+      leftFlexFlag = 1;
+    }
+  }
+
+  else if( flexPositionRight < 3300) {
+    if (rightFlexFlag == 0) {
+      Serial.println("Dodge Right");
+      rightFlexFlag = 1;
+    }
+  }
+  */
+
+  else {
+    leftAccFlag = 0;
+    rightAccFlag = 0;
+    leftFlexFlag = 0;
+    rightFlexFlag = 0;
+  }
 
   Serial.println();
  
--- acceldemoSPI/acceldemoSPI.ino
+++ acceldemoSPI/acceldemoSPI.ino
@@ -96,37 +96,49 @@ void setup(void) {
 }
 
 void loop() {
+
+  bool print_raw = false;
+  bool print_data = false;
+  
   lis.read();      // get X Y and Z data at once
   // Then print out the raw data
-  Serial.print("X:  "); Serial.print(lis.x); 
-  Serial.print("  \tY:  "); Serial.print(lis.y); 
-  Serial.print("  \tZ:  "); Serial.print(lis.z); 
+  if (print_raw) {
+    Serial.print("X:  "); Serial.print(lis.x);
+    Serial.print("  \tY:  "); Serial.print(lis.y);
+    Serial.print("  \tZ:  "); Serial.print(lis.z);
+  }
 
   lis1.read();      // get X Y and Z data at once
   // Then print out the raw data
-  //Serial.print("X1:  "); Serial.print(lis1.x); 
-  //Serial.print("  \tY1:  "); Serial.print(lis1.y); 
-  //Serial.print("  \tZ1:  "); Serial.print(lis1.z); 
+  if (print_raw) {
+    Serial.print("X1:  "); Serial.print(lis1.x);
+    Serial.print("  \tY1:  "); Serial.print(lis1.y);
+    Serial.print("  \tZ1:  "); Serial.print(lis1.z);
+  }
 
-  /* Or....get a new sensor event, normalized */ 
+  // normalize raw data values 
   sensors_event_t event; 
   lis.getEvent(&event);
 
   sensors_event_t event1; 
   lis1.getEvent(&event1);
   
-  /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("\t\tX: "); Serial.print(event.acceleration.x);
-  Serial.print(" \tY: "); Serial.print(event.acceleration.y); 
-  Serial.print(" \tZ: "); Serial.print(event.acceleration.z); 
-  Serial.println(" m/s^2 ");
+  // Display the results (acceleration is measured in m/s^2)
+  if (print_data) {
+    Serial.print("\t\tX_L: "); Serial.print(event.acceleration.x);
+    Serial.print(" \tY_L: "); Serial.print(event.acceleration.y); 
+    Serial.print(" \tZ_L: "); Serial.print(event.acceleration.z); 
+    Serial.println(" m/s^2 ");  
+  }
 
-  /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("\t\tX: "); Serial.print(event1.acceleration.x);
-  Serial.print(" \tY: "); Serial.print(event1.acceleration.y); 
-  Serial.print(" \tZ: "); Serial.print(event1.acceleration.z); 
-  Serial.println(" m/s^2 ");
-
+  // Display the results (acceleration is measured in m/s^2)
+  if (print_data) {
+    Serial.print("\t\tX_R: "); Serial.print(event1.acceleration.x);
+    Serial.print(" \tY_R: "); Serial.print(event1.acceleration.y); 
+    Serial.print(" \tZ_R: "); Serial.print(event1.acceleration.z); 
+    Serial.println(" m/s^2 ");
+  }
+  
   // Read in values of flex sensors
   int flexPositionLeft;    // Input value from the analog pin for left sensor
   int flexPositionRight;   // Input value from the analog pin for right sensor
@@ -141,69 +153,77 @@ void loop() {
   //Serial.println(flexPositionRight);
   
   //detects which punches have been thrown
-
+  
+  int key_presses = 3;
+  
   //defend - both go up at same time
-  if(event.acceleration.y < -5 && event1.acceleration.y < -5) {
-    if (leftAccFlag == 0 && rightAccFlag == 0) {
+  //if(event.acceleration.y < -5 && event1.acceleration.y < -5) {
+  if(event.acceleration.z > 8 && event1.acceleration.z > 8 && event.acceleration.x < 4 && event1.acceleration.x > -4) {
+    //if (leftAccFlag == 0 && rightAccFlag == 0) {
       Serial.println("Defend");
+      for (int count = 0; count < key_presses; count++) {
+        Serial.println(2);
+      }
       leftAccFlag = 1;
       rightAccFlag = 1;
-    }
+    //}
   }
 
   //duck - both go down at same time
-  else if(event.acceleration.y > 5 && event1.acceleration.y > 5) {
-    if (leftAccFlag == 0 && rightAccFlag == 0) {
+  //else if(event.acceleration.y > 5 && event1.acceleration.y > 5) {
+  else if(event.acceleration.z < -3 && event1.acceleration.z < -3) {
+    //if (leftAccFlag == 0 && rightAccFlag == 0) {
       Serial.println("Duck");
+      for (int count = 0; count < (2*key_presses); count++) {
+        Serial.println(2);
+      }
       leftAccFlag = 1;
       rightAccFlag = 1;
-    }
+    //}
   }
-
-  //left jab - left goes high
-  else if(event.acceleration.y < -5) {
-    if (leftAccFlag == 0) {
-      Serial.println("Left Jab");
-      leftAccFlag = 1;
-    }
-  }
-
-  //right jab - right goes high
-  else if(event1.acceleration.y < -5) {
-    if (rightAccFlag == 0) {
-      Serial.println("Right Jab");
-      rightAccFlag = 1;
-    }
-  }
-
+  
   //left body - left goes low
-  else if(event.acceleration.y > 5) {
-    if (leftAccFlag == 0) {
+  //else if(event.acceleration.y > 5) {
+  else if(event.acceleration.z > 6 && event.acceleration.x > 6) {
+    //if (leftAccFlag == 0) {
       Serial.println("Left Body");
+      for (int count = 0; count < key_presses; count++) {
+        Serial.println(8);
+      }
       leftAccFlag = 1;
-    }
+    //}
   }
 
   //right body - right goes low
-  else if(event1.acceleration.y > 5) {
-    if (rightAccFlag == 0) {
+  //else if(event1.acceleration.y > 5) {
+  else if(event1.acceleration.z > 6 && event1.acceleration.x < -6) {  
+    //if (rightAccFlag == 0) {
       Serial.println("Right Body");
+      for (int count = 0; count < key_presses; count++) {
+        Serial.println(7);
+      }
       rightAccFlag = 1;
-    }
+    //}
   }
 
-  else if( flexPositionLeft < 3200) {
-    if (leftFlexFlag == 0) {
+  else if( flexPositionLeft < 3000) {
+    //if (leftFlexFlag == 0) {
       Serial.println("Dodge Left");
+      for (int count = 0; count < key_presses; count++) {
+        Serial.println(3);
+      }
       leftFlexFlag = 1;
-    }
+    //}
   }
 
-  else if( flexPositionRight < 3300) {
-    if (rightFlexFlag == 0) {
+  else if( flexPositionRight < 2900) {
+    //if (rightFlexFlag == 0) {
       Serial.println("Dodge Right");
+      for (int count = 0; count < key_presses; count++) {
+        Serial.println(4);
+      }
       rightFlexFlag = 1;
-    }
+    //}
   }
 
   else {
@@ -211,9 +231,11 @@ void loop() {
     rightAccFlag = 0;
     leftFlexFlag = 0;
     rightFlexFlag = 0;
+    //Serial.println("Reset");
   }
-  
+
   Serial.println();
  
-  delay(300); 
+  delay(100);
 }
+
